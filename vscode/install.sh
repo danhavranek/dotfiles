6c435351ec0b8eda3a11996de6c0bf87f856fd95
Daniel Havranek <117735733+danhavranek@users.noreply.github.com>
2024-07-16 10:08:02 +0200

    feat: move keybindings and settings to separate dir

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,7 +3,7 @@
 # This script was inspired by https://github.com/CoreyMSchafer/dotfiles.git
 
 # Install VS Code extensions
-# ./install-extensions.sh
+./install-extensions.sh
 
 # Target directory for VS Code user settings on macOS
 VSCODE_USER_SETTINGS_DIR="${HOME}/Library/Application Support/Code/User/"
@@ -11,11 +11,11 @@ VSCODE_USER_SETTINGS_DIR="${HOME}/Library/Application Support/Code/User/"
 # Check if VS Code settings directory exists
 if [ -d "$VSCODE_USER_SETTINGS_DIR" ]; then
     # Backup existing settings.json and keybindings.json, if they exist
-    cp "${VSCODE_USER_SETTINGS_DIR}/settings.json" "${VSCODE_USER_SETTINGS_DIR}/settings.json.backup"
-    cp "${VSCODE_USER_SETTINGS_DIR}/keybindings.json" "${VSCODE_USER_SETTINGS_DIR}/keybindings.json.backup"
+    mv "${VSCODE_USER_SETTINGS_DIR}/settings.json" "${VSCODE_USER_SETTINGS_DIR}/settings.json.backup"
+    mv "${VSCODE_USER_SETTINGS_DIR}/keybindings.json" "${VSCODE_USER_SETTINGS_DIR}/keybindings.json.backup"
 
     # Create symlinks for settings.json and keybindings.json
-    stow --target="${VSCODE_USER_SETTINGS_DIR}" .
+    stow --target="${VSCODE_USER_SETTINGS_DIR}" settings
 
     echo "VS Code settings and keybindings have been updated."
 else
