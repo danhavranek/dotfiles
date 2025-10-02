4fbbed82846c703e74b607ccb2c80c3d393c8b65
Daniel Havranek <117735733+danhavranek@users.noreply.github.com>
2025-10-03 00:47:15 +0200

    feat: update install script

--- install.sh
+++ install.sh
@@ -18,7 +18,39 @@ stow nvim
 stow zed
 stow karabiner
 
-./vscode/install.sh
+# Nerd fonts
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-mono-nerd-font
+brew install --cask font-0xproto-nerd-font
 
 # Starship
 brew install starship
+
+# Yazi
+brew install yazi
+ya pkg insttall
+
+# VSCode
+brew install --cask visual-studio-code
+./vscode/install.sh
+
+# Bitwarden
+brew install --cask bitwarden
+
+brew install docker
+brew install --cask arc
+brew install --cask zen
+brew install --cask zed
+
+brew install neovim
+brew install lftp
+
+# MacOS specific
+brew install --cask raycast
+brew install --cask karabiner-elements
+brew install --cask alt-tab
+brew install --cask aldente
+brew install --cask monitorcontrol
+brew install --cask iterm2
+brew install --cask alacritty
+brew install --cask iina
--- vscode/install.sh
+++ vscode/install.sh
@@ -6,18 +6,18 @@
 ./install-extensions.sh
 
 # Target directory for VS Code user settings on macOS
-VSCODE_USER_SETTINGS_DIR="${HOME}/Library/Application Support/Code/User/"
+VSCODE_USER_SETTINGS_DIR="${HOME}/Library/Application Support/Code/User"
 
 # Check if VS Code settings directory exists
 if [ -d "$VSCODE_USER_SETTINGS_DIR" ]; then
-    # Backup existing settings.json and keybindings.json, if they exist
-    mv "${VSCODE_USER_SETTINGS_DIR}/settings.json" "${VSCODE_USER_SETTINGS_DIR}/settings.json.backup"
-    mv "${VSCODE_USER_SETTINGS_DIR}/keybindings.json" "${VSCODE_USER_SETTINGS_DIR}/keybindings.json.backup"
+  # Backup existing settings.json and keybindings.json, if they exist
+  mv "${VSCODE_USER_SETTINGS_DIR}/settings.json" "${VSCODE_USER_SETTINGS_DIR}/settings.json.backup"
+  mv "${VSCODE_USER_SETTINGS_DIR}/keybindings.json" "${VSCODE_USER_SETTINGS_DIR}/keybindings.json.backup"
 
-    # Create symlinks for settings.json and keybindings.json
-    stow --target="${VSCODE_USER_SETTINGS_DIR}" settings
+  # Create symlinks for settings.json and keybindings.json
+  stow --target="${VSCODE_USER_SETTINGS_DIR}" settings
 
-    echo "VS Code settings and keybindings have been updated."
+  echo "VS Code settings and keybindings have been updated."
 else
-    echo "VS Code user settings directory does not exist. Please ensure VS Code is installed."
+  echo "VS Code user settings directory does not exist. Please ensure VS Code is installed."
 fi
